a9225256ff5e0b8492ba501fecaae054cbd67b4c
Hazlo8 <alex88na@hotmail.it>
2020-02-13 12:43:00 +0100

    Added loader spinner.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,18 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoggedModule } from './private/logged.module';
 
+import { LoaderInterceptor } from './shared/interceptors/loader.interceptor';
 import { HeadersInterceptor } from './shared/interceptors/headers.interceptor';
 import { AppHttpInterceptor } from './shared/interceptors/app-http.interceptor';
 import { ErrorHttpInterceptor } from './shared/interceptors/error-http.interceptor';
 import { UserInfoService } from './shared/services/user-info.service';
 import { LoggedGuardService } from './shared/services/logged-guard.service';
+import { LoaderService } from './shared/services/loader.service';
 
 import { AppComponent } from './app.component';
 import { ErrorComponent } from './shared/components/error/error.component';
 import { ModalComponent } from './shared/components/modal/modal.component';
+import { LoaderComponent } from './shared/components/loader/loader.component';
 import { LoginComponent } from './login/login.component';
 import { NipHeaderComponent } from './nip-header/nip-header.component';
 import { SignupComponent } from './signup/signup.component';
@@ -27,6 +30,7 @@ import { HomeComponent } from './home/home.component';
     AppComponent,
     ErrorComponent,
     ModalComponent,
+    LoaderComponent,
     LoginComponent,
     NipHeaderComponent,
     SignupComponent,
@@ -45,6 +49,11 @@ import { HomeComponent } from './home/home.component';
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
+      useClass: LoaderInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
       useClass: HeadersInterceptor,
       multi: true
     },
@@ -59,7 +68,8 @@ import { HomeComponent } from './home/home.component';
       multi: true
     },
     UserInfoService,
-    LoggedGuardService
+    LoggedGuardService,
+    LoaderService
   ],
   bootstrap: [AppComponent]
 })
